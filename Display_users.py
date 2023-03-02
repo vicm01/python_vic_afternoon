61657eb61dd88a61e48f038409831b4a9a9d9e1c
vicm01 <cruisevictor007@gmail.com>
2023-03-02 14:52:44 +0300

    my first commit

--- Display_users.py
+++ Display_users.py
@@ -3,3 +3,6 @@ from main import User
 users = User.select()
 for user in users:
     print(user.user_name, user.user_phone, user.user_email, user.user_password)
+
+
+    #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
