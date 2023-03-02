7966eb0491506d3866e8cd49033e26ed67b16f02
vicm01 <cruisevictor007@gmail.com>
2023-03-02 15:58:27 +0300

    first commit

---
+++ friends.py
@@ -0,0 +1,9 @@
+friends = [ 'mwangi', 'wa iria', "Jaba", 'Juma']
+
+for me in friends:
+    print(me)
+
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ]
+
+for i in numbers:
+    print(i)
