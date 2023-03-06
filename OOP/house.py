e5cb8a6454c30bbc6bd65fccc7b0b72ebe94c5cf
vicm01 <cruisevictor007@gmail.com>
2023-03-06 16:09:28 +0300

    second commit

---
+++ OOP/house.py
@@ -0,0 +1,5 @@
+class house:
+    def   __init__(self,type,rent,owner):
+        self.type = type
+        self.rent = rent
+        self.owner = owner
---
+++ OOP/house_2.py
@@ -0,0 +1,9 @@
+from house import house
+
+house_one = house("Mansion",'$5000','Kinuthia')
+house_two = house("Apartment","$100",'Rita')
+house_three = house('Apartment','$100','Roy')
+
+print(house_one.type,house_one.type,house_one.type)
+print(house_two.type,house_two.type,house_two.type)
+print(house_three.type,house_three.type,house_three.type)
