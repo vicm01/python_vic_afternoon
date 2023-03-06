0574ac5cf47a00a4e857ae24dfe551ac5fc45e0b
vicm01 <cruisevictor007@gmail.com>
2023-03-06 16:19:14 +0300

    first commit

--- OOP/house_2.py
+++ OOP/house_2.py
@@ -7,3 +7,5 @@ house_three = house('Apartment','$100','Roy')
 print(house_one.type,house_one.type,house_one.type)
 print(house_two.type,house_two.type,house_two.type)
 print(house_three.type,house_three.type,house_three.type)
+
+#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
